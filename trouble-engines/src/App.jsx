c46c5b47d36384ac50db0fcf901a3cdf3b196cdf
samkalvium <samarth.s@kalvium.community>
2024-02-20 11:08:31 +0530

    postpage

--- trouble-engines/src/App.jsx
+++ trouble-engines/src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+
 import Home from '../components/HomePage.jsx'
+import PostPage from '../components/PostPage.jsx'
 
 function App() {
   
 
   return (
-    <>
-    <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post' element={<PostPage />} />
+      </Routes>
+    </Router>
+
+    
   )
 }
 
